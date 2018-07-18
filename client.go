9f568a1dd36e0d2175033e30c1ef82afdc5009af
Shastick <julien@sqooba.io>
2018-07-18 18:35:56 +0200

    Basic kerberos support
    This adds mutual Kerberos authentication to connections with the namenode. A
    kerberos client must be constructed manually and passed to hdfs.NewClient to
    enable support; the command-line client is currently only configurable with a
    ccache file and hadoop.security.authentication set in the hadoop configuration.

--- client.go
+++ client.go
@@ -1,6 +1,7 @@
 package hdfs
 
 import (
+	"errors"
 	"io"
 	"io/ioutil"
 	"os"
@@ -9,6 +10,7 @@ import (
 
 	hdfs "github.com/colinmarc/hdfs/protocol/hadoop_hdfs"
 	"github.com/colinmarc/hdfs/rpc"
+	krb "gopkg.in/jcmturner/gokrb5.v5/client"
 )
 
 // A Client represents a connection to an HDFS cluster
@@ -21,21 +23,72 @@ type Client struct {
 type ClientOptions struct {
 	// Addresses specifies the namenode(s) to connect to.
 	Addresses []string
-	// User specifies which HDFS user the client will act as.
+	// User specifies which HDFS user the client will act as. It is required
+	// unless kerberos authentication is enabled, in which case it will be
+	// determined from the provided credentials if empty.
 	User string
 	// Namenode optionally specifies an existing NamenodeConnection to wrap. This
 	// is useful if you needed to create the namenode net.Conn manually for
 	// whatever reason.
 	Namenode *rpc.NamenodeConnection
+	// KerberosClient is used to connect to kerberized HDFS clusters. If provided,
+	// the client will always mutually athenticate when connecting to the
+	// namenode(s).
+	KerberosClient *krb.Client
+	// KerberosServicePrincipleName specifies the Service Principle Name
+	// (<SERVICE>/<FQDN>) for the namenode(s). Like in the
+	// dfs.namenode.kerberos.principal property of core-site.xml, the special
+	// string '_HOST' can be substituted for the hostname in a multi-namenode
+	// setup (for example: 'nn/_HOST@EXAMPLE.COM'). It is required if
+	// KerberosClient is provided.
+	KerberosServicePrincipleName string
 }
 
 // ClientOptionsFromConf attempts to load any relevant configuration options
 // from the given Hadoop configuration and create a ClientOptions struct
-// suitable for creating a Client. Currently this is restricted to the namenode
-// address(es), but may be expanded in the future.
+// suitable for creating a Client. Currently this sets the following fields
+// on ClientOptions:
+//
+//   // Determined by fs.defaultFS (or the deprecated fs.default.name), or
+//   // fields beginning with dfs.namenode.rpc-address.
+//   Addresses []string
+//
+//   // Set to a non-nil but empty client (without credentials) if the value of
+//   // hadoop.security.authentication is 'kerberos'. It must then be replaced
+//   // with a credentialed Kerberos client.
+//   KerberosClient *krb.Client
+//
+//   // Determined by dfs.namenode.kerberos.principal.
+//   KerberosServicePrincipleName string
+//
+// Because of the way Kerberos can be forced by the hadoop config but not
+// actually configured, you should check for whether KerberosClient is set in
+// the resulting ClientOptions before proceeding:
+//
+//   options, _ := ClientOptionsFromConf(conf)
+//   if options.KerberosClient != nil {
+//      // Replace with a valid credentialed client.
+//      options.KerberosClient = getKerberosClient()
+//   }
 func ClientOptionsFromConf(conf HadoopConf) (ClientOptions, error) {
 	namenodes, err := conf.Namenodes()
-	return ClientOptions{Addresses: namenodes}, err
+	if err != nil && err != errNoNamenodesInConf {
+		return ClientOptions{}, err
+	}
+
+	options := ClientOptions{Addresses: namenodes}
+	if strings.ToLower(conf["hadoop.security.authentication"]) == "kerberos" {
+		// Set an empty KerberosClient here so that the user is forced to either
+		// unset it (disabling kerberos altogether) or replace it with a valid
+		// client. If the user does neither, NewClient will return an error.
+		options.KerberosClient = &krb.Client{}
+	}
+
+	if conf["dfs.namenode.kerberos.principal"] != "" {
+		options.KerberosServicePrincipleName = conf["dfs.namenode.kerberos.principal"]
+	}
+
+	return options, nil
 }
 
 // Username returns the value of HADOOP_USER_NAME in the environment, or
@@ -58,14 +111,33 @@ func Username() (string, error) {
 // the client could not be created.
 func NewClient(options ClientOptions) (*Client, error) {
 	var err error
-
 	if options.Namenode == nil {
+		if options.KerberosClient != nil && options.KerberosClient.Credentials == nil {
+			return nil, errors.New("kerberos enabled, but kerberos client is missing credentials")
+		}
+
+		if options.KerberosClient != nil && options.KerberosServicePrincipleName == "" {
+			return nil, errors.New("kerberos enabled, but kerberos namenode SPN is not provided")
+		}
+
+		if options.User == "" {
+			if options.KerberosClient != nil {
+				creds := options.KerberosClient.Credentials
+				options.User = creds.Username + "@" + creds.Realm
+			} else {
+				return nil, errors.New("user not specified")
+			}
+		}
+
 		options.Namenode, err = rpc.NewNamenodeConnectionWithOptions(
 			rpc.NamenodeConnectionOptions{
-				Addresses: options.Addresses,
-				User:      options.User,
+				Addresses:                    options.Addresses,
+				User:                         options.User,
+				KerberosClient:               options.KerberosClient,
+				KerberosServicePrincipleName: options.KerberosServicePrincipleName,
 			},
 		)
+
 		if err != nil {
 			return nil, err
 		}
@@ -76,9 +148,10 @@ func NewClient(options ClientOptions) (*Client, error) {
 
 // New returns a connected Client, or an error if it can't connect. The user
 // will be the current system user, or HADOOP_USER_NAME if set. Any relevant
-// options (including the address(es) of the namenode(s), if an empty string is
-// passed) will be loaded from the Hadoop configuration present at
-// HADOOP_CONF_DIR or the default location.
+// options (including the namenode address(es), if an empty string is passed)
+// will be loaded from the Hadoop configuration present at HADOOP_CONF_DIR or
+// the default location. Note, however, that New will not attempt any Kerberos
+// authentication; use NewClient if you need that.
 func New(address string) (*Client, error) {
 	conf := LoadHadoopConf("")
 	options, err := ClientOptionsFromConf(conf)
--- cmd/hdfs/main.go
+++ cmd/hdfs/main.go
@@ -74,8 +74,8 @@ Valid commands:
 	dfOpts = getopt.New()
 	dfh    = dfOpts.Bool('h')
 
-	cachedClient *hdfs.Client
-	status       = 0
+	cachedClients map[string]*hdfs.Client
+	status        = 0
 )
 
 func init() {
@@ -89,6 +89,8 @@ func init() {
 	duOpts.SetUsage(printHelp)
 	getmergeOpts.SetUsage(printHelp)
 	dfOpts.SetUsage(printHelp)
+
+	cachedClients = make(map[string]*hdfs.Client)
 }
 
 func main() {
@@ -170,23 +172,44 @@ func fatalWithUsage(msg ...interface{}) {
 }
 
 func getClient(namenode string) (*hdfs.Client, error) {
-	if cachedClient != nil {
-		return cachedClient, nil
+	if cachedClients[namenode] != nil {
+		return cachedClients[namenode], nil
 	}
 
 	if namenode == "" {
 		namenode = os.Getenv("HADOOP_NAMENODE")
 	}
 
-	if namenode == "" && os.Getenv("HADOOP_CONF_DIR") == "" {
+	// Ignore errors here, since we don't care if the conf doesn't exist if the
+	// namenode was specified.
+	conf := hdfs.LoadHadoopConf("")
+	options, _ := hdfs.ClientOptionsFromConf(conf)
+	if namenode != "" {
+		options.Addresses = []string{namenode}
+	}
+
+	if options.Addresses == nil {
 		return nil, errors.New("Couldn't find a namenode to connect to. You should specify hdfs://<namenode>:<port> in your paths. Alternatively, set HADOOP_NAMENODE or HADOOP_CONF_DIR in your environment.")
 	}
 
-	c, err := hdfs.New(namenode)
-	if err != nil {
-		return nil, err
+	var err error
+	if options.KerberosClient != nil {
+		options.KerberosClient, err = getKerberosClient()
+		if err != nil {
+			return nil, fmt.Errorf("Problem with kerberos authentication: %s", err)
+		}
+	} else {
+		options.User, err = hdfs.Username()
+		if err != nil {
+			return nil, fmt.Errorf("Couldn't determine user: %s", err)
+		}
 	}
 
-	cachedClient = c
-	return cachedClient, nil
+	c, err := hdfs.NewClient(options)
+	if err != nil {
+		return nil, fmt.Errorf("Couldn't connect to namenode: %s", err)
+	}
+
+	cachedClients[namenode] = c
+	return c, nil
 }
