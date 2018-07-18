0ccc32bd9e62ef49d186ae99901287391f61a126
Colin Marc <colinmarc@gmail.com>
2018-07-18 15:30:44 +0200

    Run a 'proper' hadoop cluster in travis, rather than a minicluster

--- minicluster.sh
+++ minicluster.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-HADOOP_DISTRO=${HADOOP_DISTRO-"cdh"}
+HADOOP_DISTRO=${HADOOP_DISTRO-"cdh5"}
 HADOOP_HOME=${HADOOP_HOME-"/tmp/hadoop-$HADOOP_DISTRO"}
 NN_PORT=${NN_PORT-"9000"}
 HADOOP_NAMENODE="localhost:$NN_PORT"
@@ -8,9 +8,9 @@ HADOOP_NAMENODE="localhost:$NN_PORT"
 if [ ! -d "$HADOOP_HOME" ]; then
   mkdir -p $HADOOP_HOME
 
-  if [ $HADOOP_DISTRO = "cdh" ]; then
+  if [ $HADOOP_DISTRO = "cdh5" ]; then
       HADOOP_URL="http://archive.cloudera.com/cdh5/cdh/5/hadoop-latest.tar.gz"
-  elif [ $HADOOP_DISTRO = "hdp" ]; then
+  elif [ $HADOOP_DISTRO = "hdp2" ]; then
       HADOOP_URL="http://public-repo-1.hortonworks.com/HDP/centos6/2.x/updates/2.4.0.0/tars/hadoop-2.7.1.2.4.0.0-169.tar.gz"
   else
       echo "No/bad HADOOP_DISTRO='${HADOOP_DISTRO}' specified"
@@ -26,23 +26,18 @@ fi
 
 MINICLUSTER_JAR=$(find $HADOOP_HOME -name "hadoop-mapreduce-client-jobclient*.jar" | grep -v tests | grep -v sources | head -1)
 if [ ! -f "$MINICLUSTER_JAR" ]; then
-  echo "Couldn't find minicluster jar"
+  echo "Couldn't find minicluster jar!"
   exit 1
 fi
-echo "minicluster jar found at $MINICLUSTER_JAR"
 
-
-# start the namenode in the background
-echo "Starting hadoop namenode..."
+echo "Starting minicluster..."
 $HADOOP_HOME/bin/hadoop jar $MINICLUSTER_JAR minicluster -nnport $NN_PORT -datanodes 3 -nomr -format "$@" > minicluster.log 2>&1 &
-sleep 30
+echo "Waiting for namenode to start up..."
+$HADOOP_HOME/bin/hdfs dfsadmin "-Dfs.defaultFS=$HADOOP_NAMENODE" -safemode wait
 
-HADOOP_FS="$HADOOP_HOME/bin/hadoop fs -Ddfs.block.size=1048576"
-$HADOOP_FS -mkdir -p "hdfs://$HADOOP_NAMENODE/_test"
-$HADOOP_FS -chmod 777 "hdfs://$HADOOP_NAMENODE/_test"
+HADOOP_FS="$HADOOP_HOME/bin/hadoop fs"
+./fixtures.sh
 
-$HADOOP_FS -put ./testdata/foo.txt "hdfs://$HADOOP_NAMENODE/_test/foo.txt"
-$HADOOP_FS -put ./testdata/mobydick.txt "hdfs://$HADOOP_NAMENODE/_test/mobydick.txt"
-
-echo "Please run the following command:"
+echo "Please run the following commands:"
 echo "export HADOOP_NAMENODE='$HADOOP_NAMENODE'"
+echo "export HADOOP_FS='$HADOOP_HOME/bin/hadoop'"
